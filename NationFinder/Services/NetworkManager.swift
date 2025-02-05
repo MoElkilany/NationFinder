5071b604b9b7c5458eadc6c5c7258a50ee61233d
mohamed Elkilany <mohamedsayedelkilany@gmail.com>
2025-02-05 21:00:17 +0200

    Create NetworkManagerProtocol and implement conforming class - Defined NetworkManagerProtocol to  necessary network methods 🛠️ - Added basic network request functionality for API integration

---
+++ NationFinder/Services/NetworkManager.swift
@@ -0,0 +1,22 @@
+//
+//  NetworkManager.swift
+//  YourCountryApp
+//
+//  Created by Mohamed Elkilany on 05/02/2025.
+//
+
+import Foundation
+
+protocol NetworkManagerProtocol {
+    func fetchData(from url: URL) async throws -> Data
+}
+
+class NetworkManager: NetworkManagerProtocol {
+    func fetchData(from url: URL) async throws -> Data {
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw AppError.noData
+        }
+        return data
+    }
+}
