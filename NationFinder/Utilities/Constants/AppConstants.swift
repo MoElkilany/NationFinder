027004e8e1a3787e6587f5a5495a72b2e071e77e
mohamed Elkilany <mohamedsayedelkilany@gmail.com>
2025-02-05 20:57:18 +0200

    Create Utilities folder with AppConstants and Enums for AppError
    - Created AppConstants file for storing constants
    - Created Enums folder containing AppError enum to handle error cases

---
+++ NationFinder/Utilities/Constants/AppConstants.swift
@@ -0,0 +1,14 @@
+//
+//  AppConstants.swift
+//  YourCountryApp
+//
+//  Created by Mohamed Elkilany on 05/02/2025.
+//
+
+import Foundation
+
+struct AppConstants {
+    struct API {
+        static let countriesEndpoint = "https://restcountries.com/v2/all"
+    }
+}
---
+++ NationFinder/Utilities/Enums/AppError.swift
@@ -0,0 +1,28 @@
+//
+//  AppError.swift
+//  PreFinalChallange
+//
+//  Created by Mohamed Elkilany on 05/02/2025.
+//
+
+import Foundation
+
+enum AppError: Error, LocalizedError {
+    case noData
+    case urlError
+    case decodingError
+    case networkError(Error)
+
+    var errorDescription: String? {
+        switch self {
+        case .noData:
+            return "No data received from the server."
+        case .urlError:
+            return "Invalid URL."
+        case .decodingError:
+            return "Failed to decode data."
+        case .networkError(let error):
+            return "Network error: \(error.localizedDescription)"
+        }
+    }
+}
