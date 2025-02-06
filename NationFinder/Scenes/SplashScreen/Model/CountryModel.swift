33966606be9ecfb2e4effc7a5ad5bbce7032b48c
mohamed Elkilany <mohamedsayedelkilany@gmail.com>
2025-02-06 20:32:32 +0200

    Convert models to use Swift Data for better data management🔧

--- NationFinder/Scenes/SplashScreen/Model/CountryModel.swift
+++ NationFinder/Scenes/SplashScreen/Model/CountryModel.swift
@@ -7,23 +7,87 @@
 
 
 import Foundation
+import SwiftData
 
-struct CountryModel: Codable,Hashable{
-    let name, capital: String?
-    let currencies: [Currency]?
-    let latlng: [Double]?
-    let flags: FlagsModel?
-    let languages: [LanguagesModel]?
+@Model
+class CountryModel: Decodable  {
+
+    var name: String?
+    var capital: String?
+    var currencies: [Currency]?
+    var latlng: [Double]?
+    var flags: FlagsModel?
+    var languages: [LanguagesModel]?
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case capital
+        case currencies
+        case latlng
+        case flags
+        case languages
+
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decodeIfPresent(String.self, forKey: .name)
+        capital = try container.decodeIfPresent(String.self, forKey: .capital)
+        currencies = try container.decodeIfPresent([Currency].self, forKey: .currencies)
+        latlng = try container.decodeIfPresent([Double].self, forKey: .latlng)
+        flags = try container.decodeIfPresent(FlagsModel.self, forKey: .flags)
+        languages = try container.decodeIfPresent([LanguagesModel].self, forKey: .languages)
+
+    }
+    
+    
+     
 }
 
-struct Currency: Codable,Hashable {
-    let code, name, symbol: String?
+@Model
+class Currency: Decodable {
+    var code: String?
+    var name: String?
+    var symbol: String?
+
+    enum CodingKeys: String, CodingKey {
+        case code
+        case name
+        case symbol
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        code = try container.decodeIfPresent(String.self, forKey: .code)
+        name = try container.decodeIfPresent(String.self, forKey: .name)
+        symbol = try container.decodeIfPresent(String.self, forKey: .symbol)
+    }
 }
 
-struct FlagsModel: Codable,Hashable {
-    let png: String?
+@Model
+class FlagsModel: Decodable {
+    var png: String?
+
+    enum CodingKeys: String, CodingKey {
+        case png
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        png = try container.decodeIfPresent(String.self, forKey: .png)
+    }
 }
 
-struct LanguagesModel: Codable,Hashable {
-    let name: String?
+@Model
+class LanguagesModel: Decodable {
+    var name: String?
+
+    enum CodingKeys: String, CodingKey {
+        case name
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decodeIfPresent(String.self, forKey: .name)
+    }
 }
